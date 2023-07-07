731562a6510464845cf8d5cd8c563c4cc1369d8b
Niclas Tanskanen <niclas@niclass-air.home>
2023-07-07 14:44:26 +0200

    add UserButton for clerk at root page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,11 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs"
 
-export default function Home() {
-    return (
-      <div className="p-4">
-        <Button>Click me</Button>
-      </div>
-    )
-  }
+const SetupPage = () => {
+  return (
+    <div>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  )
+}
+  
+export default SetupPage
